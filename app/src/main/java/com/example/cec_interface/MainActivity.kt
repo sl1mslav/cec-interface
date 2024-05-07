cd8790906d229f9e6731cde8c6fc3703963c7571
sl1mslav <dm.partilov@gmail.com>
2024-05-07 16:46:12 +0500

    test out shell script logic

--- app/src/main/java/com/example/cec_interface/MainActivity.kt
+++ app/src/main/java/com/example/cec_interface/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.cec_interface
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import android.service.controls.DeviceTypes
-import android.widget.TextView
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.cec_interface.databinding.ActivityMainBinding
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStreamReader
+import kotlin.time.Duration.Companion.seconds
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,17 +24,89 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val libCEC = LibCEC.initialize(true)
-        println("yeah!")
+        //val libCEC = LibCEC.initialize(true)
+        //launchCecClient()
+        //binding.sampleText.text = libCEC.isTVOn.toString()
         lifecycleScope.launch {
-            while (true) {
-                binding.sampleText.text = "Powering off TV..."
-                libCEC.powerOffTV()
-                delay(5000L)
-                binding.sampleText.text = "Powering on TV..."
-                delay(5000L)
+            delay(5.seconds)
+            Runtime.getRuntime().exec(arrayOf("su", "echo 0x40 0x36 > /sys/class/cec/cmd"))
+        }
+        //runtime.exec("adb root")
+        //runtime.exec("echo 0x40 0x36 > /sys/class/cec/cmd")
+        binding.turnTvOn.setOnClickListener {
+            executeCommandAndLogOutput(command =  "adb shell \"su echo 0x40 0x04 > /sys/class/cec/cmd\"")
+        }
+        binding.turnTvOff.setOnClickListener {
+            executeCommandAndLogOutput(command = "adb shell su \"echo 0xF0 0x36 > /sys/class/cec/cmd\"")
+            executeCommandAndLogOutput(command = "adb shell su \"echo 0x40 0x36 > /sys/class/cec/cmd\"")
+        }
+    }
+
+    private fun launchCecClient() {
+        val cecName = if (Build.SUPPORTED_ABIS.contains("x86_64")) "cec_client.sh" else "cec_client_arm.sh"
+        val pathToScript = getDir("cec_scripts", 0).absolutePath + File.separator + cecName
+        val `in` = resources.openRawResource(R.raw.cec_client)
+        var out: FileOutputStream? = null
+        try {
+            out = FileOutputStream(pathToScript)
+            val buff = ByteArray(1024)
+            var read: Int
+            while ((`in`.read(buff).also { read = it }) > 0) {
+                out.write(buff, 0, read)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            try {
+                `in`.close()
+                out!!.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
         }
-        libCEC.powerOnTV()
+        executeCommandAndLogOutput(command = "chmod 777 $pathToScript")
+        executeCommandAndLogOutput(command = "echo $pathToScript")
+    }
+
+    private fun executeCommandAndLogOutput(
+        runtime: Runtime = Runtime.getRuntime(),
+        command: String
+    ) {
+        var reader: BufferedReader? = null
+        var result = ""
+        try {
+            val p = runtime.exec(command)
+
+            /*Timer().schedule(object : TimerTask() {
+                override fun run() {
+                    reader = BufferedReader(InputStreamReader(p.inputStream))
+                    var line: String?
+
+                    while ((reader!!.readLine().also { line = it }) != null) {
+                        result += line + "\n"
+                        Log.i("Test ", result)
+                    }
+                }
+
+            },1000,1000)*/
+
+            reader = BufferedReader(InputStreamReader(p.inputStream))
+            var line: String?
+
+            while ((reader.readLine().also { line = it }) != null) {
+                result += line + "\n"
+            }
+
+            p.waitFor()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        } finally {
+            if (reader != null) try {
+                reader.close()
+            } catch (e: java.lang.Exception) {
+                e.printStackTrace()
+            }
+        }
+        Log.d("TAG", "executeCommandAndLogOutput: $result")
     }
 }
